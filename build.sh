2a71ad5e5f2181e608efa8385e59803fa4425ddb
likkai <brandchong2004@gmail.com>
2024-09-15 22:20:55 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+# clang downloaded from: https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86
+# linux-x86 is inside toolchains folder
+
+make -j$(nproc --all) O=out ARCH=arm64 lisa_defconfig \
+  LLVM=1 \
+  LLVM_IAS=1 \
+  HOSTLD=ld.lld \
+  CC=clang \
+  CROSS_COMPILE=aarch64-linux-gnu- \
+  CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
+
+make -j$(nproc --all) O=out ARCH=arm64 \
+  LLVM=1 \
+  LLVM_IAS=1 \
+  HOSTLD=ld.lld \
+  CC=clang \
+  CROSS_COMPILE=aarch64-linux-gnu- \
+  CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
