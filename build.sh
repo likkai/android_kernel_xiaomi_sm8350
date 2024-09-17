c509758e69d212556516d2ac29faf58e1b48b9f2
likkai <brandchong2004@gmail.com>
2024-09-17 08:57:57 +0800

    Modify build.sh and integrate ksu

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 # clang downloaded from: https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86
 # linux-x86 is inside toolchains folder
 
+export PATH=~/toolchains/linux-x86/clang-r498229b/bin:$PATH
+
 make -j$(nproc --all) O=out ARCH=arm64 lisa_defconfig \
   LLVM=1 \
   LLVM_IAS=1 \
